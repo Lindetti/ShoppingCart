6c24ef9035423240bb7bc30713886404179d3d7a
Alexander Lind <alexander_lind95@hotmail.com>
2023-04-17 23:56:45 +0200

    update

---
+++ ShoppingCart/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav>
+        <div className="github">
+        </div>
+        <div className="cart">
+       <button className="cartBtn">Cart</button>
+        </div>
+        </nav>
+    )
+}
+
+export default Navbar;
